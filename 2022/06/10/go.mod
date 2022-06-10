caa2a2f919f10f99bcd90bb77bcd8c971cfac23a
Mario Carrion <mario@carrion.ws>
2022-06-10 01:40:12 -0400

    Software Architecture in Go: Orchestration Pattern
    https://youtu.be/jPVz1Y4_2k4

---
+++ 2022/06/10/go.mod
@@ -0,0 +1,5 @@
+module github.com/MarioCarrion/videos/2022/06/orchestration
+
+go 1.18
+
+require golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
---
+++ 2022/06/10/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"sync/atomic"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type Service func(ctx context.Context) (int64, error)
+
+func main() {
+	svcs := []Service{
+		Service1,
+		Service2,
+		Service3,
+	}
+
+	var total int64
+
+	start := time.Now()
+
+	g, ctx := errgroup.WithContext(context.Background())
+
+	for _, svc := range svcs {
+		g.Go(func() error {
+			v, err := svc(ctx)
+			if err != nil {
+				return err
+			}
+
+			_ = atomic.AddInt64(&total, v)
+
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		log.Fatalln("Group", err)
+	}
+
+	fmt.Println("Total", total, "Duration", time.Now().Sub(start))
+}
+
+func Service1(ctx context.Context) (int64, error) {
+	time.Sleep(100 * time.Millisecond)
+	return 1, nil
+}
+
+func Service2(ctx context.Context) (int64, error) {
+	time.Sleep(100 * time.Millisecond)
+	return 2, nil
+}
+
+func Service3(ctx context.Context) (int64, error) {
+	time.Sleep(200 * time.Millisecond)
+	return 3, nil
+}
